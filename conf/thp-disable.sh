cf281eb02a95b86b13e284356469b8ac530e0db6
Alexey Bychko <abychko@gmail.com>
2015-05-27 17:44:27 +0600

    added script file for disabling THP

---
+++ conf/thp-disable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+PATH="${PATH}:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin"
+#
+if [ -f /sys/kernel/mm/transparent_hugepage/enabled ]; then
+   echo never > /sys/kernel/mm/transparent_hugepage/enabled
+fi
+#
+if [ -f /sys/kernel/mm/transparent_hugepage/defrag]; then
+	echo never > /sys/kernel/mm/transparent_hugepage/defrag
+fi
+#
